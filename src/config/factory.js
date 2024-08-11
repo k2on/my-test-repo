da1167dd527c58b91f1bde6730cb9549be4aaf67
Max Koon <theakman96@gmail.com>
2024-08-11 07:48:34 -0400

    Delete src directory

--- src/config/factory.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createConfig = void 0;
-const createConfig = (config) => config;
-exports.createConfig = createConfig;
-//# sourceMappingURL=factory.js.map
