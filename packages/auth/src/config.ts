a2564c6609059f7a9d9fb9934226138c3eead4bc
Max Koon <theakman96@gmail.com>
2024-08-12 13:49:44 -0400

    Commit from Expo

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -1,1 +1,13 @@
-got config: {"providers":["github"]}
+import { createConfig } from "@koons/auth";
+import { Github } from "@koons/github";
+
+const github = Github({
+    clientId: process.env.EXPO_PUBLIC_GITHUB_CLIENT_ID ?? "NOTDEFINED",
+    clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "NOTDEFINED",
+});
+
+export const authConfig = createConfig({
+    providers: {
+        oauth: { github },
+    },
+});
