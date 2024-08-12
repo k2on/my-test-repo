ec862595e99806ca89e9e9b3a1beda00981074f3
Max Koon <theakman96@gmail.com>
2024-08-11 20:18:06 -0400

    Delete auth directory

--- auth/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { authConfig } from "./config";
-
-type AuthConfig = typeof authConfig;
-
-export { authConfig, type AuthConfig };
-export * as types from "./config/types";
-export { OAuthProvider } from "./oauth";
